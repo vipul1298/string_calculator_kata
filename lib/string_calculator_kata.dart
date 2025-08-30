84a81e6b82c800bc9d56605a95bac720c8b831e3
Vipul Chaurasia <vipulchaurasia@Mac.lan>
2025-08-30 12:46:50 +0530

    Basic add function and test for empty string

--- lib/string_calculator_kata.dart
+++ lib/string_calculator_kata.dart
@@ -1,3 +1,5 @@
-int calculate() {
-  return 6 * 7;
+class StringCalculator {
+  int add(String numbers){
+    return 0;
+  }
 }
--- test/string_calculator_kata_test.dart
+++ test/string_calculator_kata_test.dart
@@ -1,8 +1,13 @@
+import 'dart:math';
+
 import 'package:string_calculator_kata/string_calculator_kata.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+group("StringCalculator", (){
+  final calculator = StringCalculator();
+  test('empty string returns 0', (){
+    expect(calculator.add(''), equals(0));
   });
+});
 }
