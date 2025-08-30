22a07f1fa020d46b2539a3a3ab6c91e32c40998b
Vipul Chaurasia <vipulchaurasia@Mac.lan>
2025-08-30 12:33:46 +0530

    initial project setup

---
+++ bin/string_calculator_kata.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator_kata/string_calculator_kata.dart' as string_calculator_kata;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calculator_kata.calculate()}!');
+}
---
+++ test/string_calculator_kata_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calculator_kata/string_calculator_kata.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
